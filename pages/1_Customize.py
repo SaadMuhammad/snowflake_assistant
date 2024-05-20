fedf4dabb0fe7af5d6dcf9cfe938134fcb6c837b
Muhammad Saad Uddin <51073248+SaadMuhammad@users.noreply.github.com>
2024-05-20 10:12:31 +0200

    Update 1_Customize.py

--- pages/1_Customize.py
+++ pages/1_Customize.py
@@ -7,20 +7,3 @@ from utils.logo import add_logo
 
 
 st.set_page_config(page_title="SnowFlake Assitant", page_icon="❄️🔍", layout="wide")
-
-
-logo_data = add_logo()
-
-st.markdown(
-    f"""
-    <style>
-        [data-testid="stSidebarNav"] {{
-            background-image: url(data:image/png;base64,{logo_data});
-            background-repeat: no-repeat;
-            padding-top: 120px;
-            background-position: 20px 20px;
-        }}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
